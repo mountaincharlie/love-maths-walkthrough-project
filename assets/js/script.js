0659fac84c0151f876657b2866eb174ca62a2816
Charlie <ciharland@hotmail.co.uk>
2021-11-19 10:56:53 +0000

    Create game functions structure in the js file

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// function for ...
+function runGame(){
+    
+}
+
+// function for checking the user's answer
+function checkAnswer(){
+    
+}
+
+// function for ...
+function calculateCorrectAnswer(){
+    
+}
+
+// function for calculating the user's score
+function incrementScore(){
+    
+}
+
+// function for counting the user's incorrect answers
+function incrementWrongAnswers(){
+    
+}
+
+// function for displaying the addition question
+function displayAdditionQuestion(){
+    
+}
+
+// function for displaying the subtract question
+function displaySubtractQuestion(){
+    
+}
+
+// function for displaying the multiply question
+function displayMultiplyQuestion(){
+    
+}
+
+// TO COMPLETE - function for displaying the divide question
+// function displayDivideQuestion(){}
