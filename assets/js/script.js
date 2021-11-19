074946ecb23e4a84ef650f9d6e35f4dad8ad5e7d
Charlie <ciharland@hotmail.co.uk>
2021-11-19 11:14:50 +0000

    Add event listeners for the buttons to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+// --- EVENT LISTENERS ---
+
+// wait for the DOM to finish loading before running the game
+// get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getELementsByTagName("button"); // returns an array (html collection) of the buttons
+
+    // iterating through the buttons array by storing/using each value in buttons one per iteration (dont need indexing)
+    for (button in buttons){
+
+        // ev-lis click function on each button
+        button.addEventListener("click", function(){
+
+            // finding out the data type from the attribute we created where 'this' refers to the button in this iteration 
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit!");
+            } else {
+                // otherwise setting the data-type as the gaem type
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+
+        });
+    }
+
+})
+
+
+
+
+
+// --- FUNCTIONS ---
+
 // function for ...
 function runGame(){
     
